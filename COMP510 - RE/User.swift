848c2229e4e66f3ba749cace3aafc53d238b5594
Jamone Kelly <xg.monie089@gmail.com>
2015-09-24 23:47:24 -0400

    Create User model.

---
+++ COMP510 - RE/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  COMP510 - RE
+//
+//  Created by Jamone Alexander Kelly on 9/24/15.
+//  Copyright © 2015 COMP510. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+
+class User {
+    
+}
