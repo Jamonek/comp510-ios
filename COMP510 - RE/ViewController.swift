97d50e0dc2394d4bde5f56912643dfbd68857b08
Jamone Kelly <xg.monie089@gmail.com>
2015-09-24 21:29:39 -0400

    Initial Commit

---
+++ COMP510 - RE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  COMP510 - RE
+//
+//  Created by Jamone Alexander Kelly on 9/24/15.
+//  Copyright © 2015 COMP510. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
